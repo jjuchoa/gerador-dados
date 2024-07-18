638dcdfaca92f93e0251add3732f1b174c5402c6
jjuchoa <jbfdujunior@gmail.com>
2024-07-18 16:54:45 -0300

    Inicio do ajuste do calculo do DV

--- src/calculoDV.js
+++ src/calculoDV.js
@@ -1,21 +1,47 @@
 function calcularDigitoVerificador(numero) {
-    // Verifica se o número é uma string
-    if (typeof numero !== 'string') {
-        throw new Error('O número deve ser uma string.');
+    
+    // Verifica se o valor recebido é uma string 
+    if  (typeof numero !== 'string') {
+       numeroString = numero.toString();
+    } else {
+        numeroString = numero;
     }
 
     // Remove espaços em branco
-    numero = numero.replace(/\s+/g, '');
+    numeroString = numeroString.replace(/\s+/g, '');
+    console.log(numeroString);
 
-    // Concatena o número com "00"
-    const numeroConcatenado = numero + '00';
+    // Armazena o valor do processo (NNNNNNN)
+    const processo = numeroString.slice(0, 7)
+    console.log(processo);
+
+    // Concatena o ano com poder judiciário e tribunal (AAAAJTR)
+    const anoConcatenado = numeroString.slice(7, 14);
+    console.log(anoConcatenado);
+
+    // Concatena o unidade organica + "00" (OOOO00)
+    //const unidadeOrganica = numeroString.slice(14, 18) + "00";
+    const unidadeOrganica = numeroString.slice(14, 18);
+    console.log(unidadeOrganica);
 
     // Converte para um número inteiro
-    const numeroInt = BigInt(numeroConcatenado);
+   const processoInt = BigInt(processo);
+    const anoConcatenadoInt = BigInt(anoConcatenado);
+    const unidadeOrganicaInt = BigInt(unidadeOrganica);
+
+    const R1 = processoInt % BigInt(97);
+    console.log(R1);
+
+    const R2 = BigInt(R1 + anoConcatenadoInt) % BigInt(97);
+    console.log(R2);
+
+    const R3 = BigInt(R2 + unidadeOrganicaInt) % BigInt(97);
+    console.log(R3);
 
     // Calcula o módulo 97
-    const resto = numeroInt % BigInt(97);
-    const digitoVerificador = (BigInt(97) - resto) % BigInt(97);
+    //const resto = processoInt % BigInt(97);
+    //const digitoVerificador = (BigInt(97) - resto) % BigInt(97);
+    const digitoVerificador = BigInt(98) - R3;
 
     // Formata o dígito verificador com dois dígitos
     return String(digitoVerificador).padStart(2, '0');
@@ -23,3 +49,4 @@ function calcularDigitoVerificador(numero) {
 
 module.exports = calcularDigitoVerificador;
 
+console.log(calcularDigitoVerificador('180740020244036301'))
--- src/gerarNumeroProcesso.js
+++ src/gerarNumeroProcesso.js
@@ -3,13 +3,15 @@ function gerarNumeroProcesso(numero) {
     const data = new Date();
 
     const NNNNNNN = String(new Date().getDate()).padStart(2, '0')+String(new Date().getMonth()).padStart(2, '0')+String(numero);
-    const DD = calcularDigitoVerificador(NNNNNNN);
     const AAAA = new Date().getFullYear();
     const J = '4';
     const TR = '03';
     const OOOO = '6301';
+    const numeroSemDD = NNNNNNN + AAAA + J + TR + OOOO;
+    const DD = calcularDigitoVerificador(numeroSemDD);
+    const numeroComDD = NNNNNNN + DD + AAAA + J + TR + OOOO;
     
-    return `${NNNNNNN}-${DD}.${AAAA}.${J}.${TR}.${OOOO}`;
+    return "Numero Processo Judicial: "+`${NNNNNNN}-${DD}.${AAAA}.${J}.${TR}.${OOOO}`+" | "+numeroComDD;
 }
 
 module.exports = gerarNumeroProcesso;
